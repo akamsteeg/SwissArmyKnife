ea764f9e86c1e8a0827b4658e26c9b8a8bbd9f89
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 22:51:31 +0200

    Set all values in AssemblyInfo

--- src/SwissArmyKnife/Properties/AssemblyInfo.cs
+++ src/SwissArmyKnife/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SwissArmyKnife")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SwissArmyKnife")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Alex Kamsteeg")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +31,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Assembly is fully CSL compliant
+[assembly: CLSCompliant(true)]

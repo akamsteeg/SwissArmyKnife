4f92387fc9622c3c1918fa56704c6db60fe53cf1
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 18:31:59 +0200

    Added a benchmarking project

---
+++ src/SwissArmyKnife.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using Minibench.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Benchmarks
+{
+    class Program
+    {
+        private static int Main(string[] args)
+        {
+            var run = BenchmarkRunner.RunFromCommandLine(typeof(Program).Assembly);
+            return run == null ? 1 : 0;
+        }
+    }
+}

f176f4687f1c4938dd862dfa43974f054296e0f4
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 18:13:14 +0200

    Added extension methods for String & StringBuilder

---
+++ src/SwissArmyKnife/StringBuilderExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace SwissArmyKnife
+{
+    /// <summary>
+    /// </summary>
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Appends a line by processing a composite format string, which
+        /// contains zero of more format items
+        /// </summary>
+        /// <param name="provider">
+        /// An object that supplies culture-specific formatting information
+        /// </param>
+        /// <param name="format">
+        /// A composite format string
+        /// </param>
+        /// <param name="args">
+        /// An object array that contains zero or more objects to format
+        /// </param>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, IFormatProvider provider, string format, params object[] args)
+        {
+            sb.AppendFormat(provider, format, args);
+            return sb.AppendLine();
+        }
+
+        /// <summary>
+        /// Appends a line by processing a composite format string, which
+        /// contains zero of more format items
+        /// </summary>
+        /// <param name="format">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The object to format
+        /// </param>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0)
+        {
+            sb.AppendFormat(format, arg0);
+            return sb.AppendLine();
+        }
+
+        /// <summary>
+        /// Appends a line by processing a composite format string, which
+        /// contains zero of more format items
+        /// </summary>
+        /// <param name="format">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The first object to format
+        /// </param>
+        /// <param name="arg1">
+        /// The second object to format
+        /// </param>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1)
+        {
+            sb.AppendFormat(format, arg0, arg1);
+            return sb.AppendLine();
+        }
+
+        /// <summary>
+        /// Appends a line by processing a composite format string, which
+        /// contains zero of more format items
+        /// </summary>
+        /// <param name="format">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The first object to format
+        /// </param>
+        /// <param name="arg1">
+        /// The second object to format
+        /// </param>
+        /// <param name="arg2">
+        /// The third object to format
+        /// </param>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, object arg0, object arg1, object arg2)
+        {
+            sb.AppendFormat(format, arg0, arg1, arg2);
+            return sb.AppendLine();
+        }
+
+        /// <summary>
+        /// Appends a line by processing a composite format string, which
+        /// contains zero of more format items
+        /// </summary>
+        /// <param name="provider">
+        /// An object that supplies culture-specific formatting information
+        /// </param>
+        /// <param name="format">
+        /// A composite format string
+        /// </param>
+        /// <param name="args">
+        /// An object array that contains zero or more objects to format
+        /// </param>
+        public static StringBuilder AppendFormatLine(this StringBuilder sb, string format, params object[] args)
+        {
+            sb.AppendFormat(format, args);
+            return sb.AppendLine();
+        }
+    }
+}
---
+++ src/SwissArmyKnife/StringExtensions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace SwissArmyKnife
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Replaces the format item in a specified string with the string
+        /// representation of the specified object
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The object to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which any format items are replaced by the
+        /// string representation of arg0
+        /// </returns>
+        public static string FormatThis(this string source, object arg0)
+        {
+            return string.Format(source, arg0);
+        }
+
+        /// <summary>
+        /// Replaces the format items in a specified string with the string
+        /// representation of two specified objects
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The first object to format
+        /// </param>
+        /// <param name="arg1">
+        /// The second object to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which any format items are replaced by the
+        /// string representation of arg0 and arg1
+        /// </returns>
+        public static string FormatThis(this string source, object arg0, object arg1)
+        {
+            return string.Format(source, arg0, arg1);
+        }
+
+        /// <summary>
+        /// Replaces the format items in a specified string with the string
+        /// representation of three specified objects
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The first object to format
+        /// </param>
+        /// <param name="arg1">
+        /// The second object to format
+        /// </param>
+        /// <param name="arg2">
+        /// The third object to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which any format items are replaced by the
+        /// string representation of arg0, arg1 and arg2
+        /// </returns>
+        public static string FormatThis(this string source, object arg0, object arg1, object arg2)
+        {
+            return string.Format(source, arg0, arg1, arg2);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a specified string with the string
+        /// representation of a corresponding object in a specified array
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="args">
+        /// An object array that contains zero or more objects to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which the format items have been replaced by
+        /// the string representation of the corresponding objects in args
+        /// </returns>
+        public static string FormatThis(this string source, params object[] args)
+        {
+            return string.Format(source, args);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a specified string with the string
+        /// representation of a corresponding object in a specified array
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="formatProvider">
+        /// An object that supplies culture-specific formatting information
+        /// </param>
+        /// <param name="args">
+        /// An object array that contains zero or more objects to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which the format items have been replaced by
+        /// the string representation of the corresponding objects in args
+        /// </returns>
+        public static string FormatThis(this string source, IFormatProvider formatProvider, params object[] args)
+        {
+            return string.Format(formatProvider, source, args);
+        }
+    }
+}

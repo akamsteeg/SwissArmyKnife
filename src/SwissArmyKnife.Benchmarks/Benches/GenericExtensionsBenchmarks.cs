9555019ca4cbb118a8a8f5aad8ee7ab4ce565718
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 20:14:49 +0200

    Added benchmarks for IsBetween()

--- src/SwissArmyKnife.Benchmarks/Benches/GenericExtensionsBenchmarks.cs
+++ src/SwissArmyKnife.Benchmarks/Benches/GenericExtensionsBenchmarks.cs
@@ -84,5 +84,27 @@ namespace SwissArmyKnife.Benchmarks.Benches
         }
 
         #endregion
+
+        #region IsBetween()
+
+        [Benchmark]
+        public void StringIsBetween()
+        {
+            var t1 = "b".IsBetween("a", "b");
+        }
+
+        [Benchmark]
+        public void IntIsBetween()
+        {
+            var t1 = 1.IsBetween(0, 2);
+        }
+
+        [Benchmark]
+        public void DoubleIsBetween()
+        {
+            var t1 = 1.0.IsBetween(0.0, 1.1);
+        }
+
+        #endregion
     }
 }

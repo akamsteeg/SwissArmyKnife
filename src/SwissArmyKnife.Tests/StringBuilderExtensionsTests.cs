d8a4bbf44da0fc155cb50f39e9e5f771864661b7
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 18:27:31 +0200

    Added unit tests for the String & StringBuilder extension methods

---
+++ src/SwissArmyKnife.Tests/StringBuilderExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwissArmyKnife;
+using NUnit.Framework;
+
+namespace SwissArmyKnife.Tests
+{
+    [TestFixture]
+    class StringBuilderExtensionsTests
+    {
+        [Test]
+        public void AppendFormattedLineOneArg_Successful()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormatLine("Test: {0}", "0");
+
+            var expected = string.Format("Test: 0{0}", Environment.NewLine);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [Test]
+        public void AppendFormattedLineTwoArgs_Successful()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormatLine("Test: {0} {1}", "0", "1");
+
+            var expected = string.Format("Test: 0 1{0}", Environment.NewLine);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [Test]
+        public void AppendFormattedLineThreeArgs_Successful()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormatLine("Test: {0} {1} {2}", "0", "1", "2");
+
+            var expected = string.Format("Test: 0 1 2{0}", Environment.NewLine);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+        [Test]
+        public void AppendFormattedLineManyArgs_Successful()
+        {
+            var sb = new StringBuilder();
+            sb.AppendFormatLine("Test: {0} {1} {2} {3} {4}", "0", "1", "2", "3", "4");
+
+            var expected = string.Format("Test: 0 1 2 3 4{0}", Environment.NewLine);
+            Assert.AreEqual(expected, sb.ToString());
+        }
+
+    }
+}
---
+++ src/SwissArmyKnife.Tests/StringExtensionsTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwissArmyKnife;
+using NUnit.Framework;
+using System.Threading;
+using System.Globalization;
+
+namespace SwissArmyKnife.Tests
+{
+    [TestFixture]
+    class StringExtensionsTests
+    {
+        [Test]
+        public void FormatStringWithOneArg_Successful()
+        {
+            var s = "Test: {0}".FormatThis("1");
+
+            Assert.AreEqual("Test: 1", s);
+        }
+
+        [Test]
+        public void FormatStringWithTwoArgs_Successful()
+        {
+            var s = "Test: {0} {1}".FormatThis("1", "2");
+
+            Assert.AreEqual("Test: 1 2", s);
+        }
+
+        [Test]
+        public void FormatStringWithThreeArgs_Successful()
+        {
+            var s = "Test: {0} {1} {2}".FormatThis("1", "2", "3");
+
+            Assert.AreEqual("Test: 1 2 3", s);
+        }
+
+        [Test]
+        public void FormatStringWithManyArgs_Successful()
+        {
+            var s = "Test: {0} {1} {2} {3} {4} {5}".FormatThis("1", "2", "3", "4", "5", "6");
+
+            Assert.AreEqual("Test: 1 2 3 4 5 6", s);
+        }
+
+        [Test]
+        public void FormatStringWithToManyArgs_Successful()
+        {
+            var s = "Test: {0} {1} {2} {3} {4}".FormatThis("1", "2", "3", "4", "5", "6");
+
+            Assert.AreEqual("Test: 1 2 3 4 5", s);
+        }
+
+        [Test]
+        [ExpectedException(typeof(FormatException))]
+        public void FormatStringWithNotEnoughArgs_Throws()
+        {
+            var s = "Test: {0} {1} {2} {3} {4} {5}".FormatThis("1", "2", "3", "4", "5");
+        }
+
+        [Test]
+        public void FormatStringWithOneArgFormattedTwice_Successful()
+        {
+            var s = "Test: {0} {0}".FormatThis("1");
+
+            Assert.AreEqual("Test: 1 1", s);
+        }
+
+        #region Types
+
+        [Test]
+        public void FormatStringWithIntegerAsArg_Successful()
+        {
+            var s = "Test: {0}".FormatThis(1);
+
+            Assert.AreEqual("Test: 1", s);
+        }
+
+        [Test]
+        public void FormatStringWithDoubleAsArg_Successful()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; // Ensure a dot in the string representation of a double
+
+            var s = "Test: {0}".FormatThis(1.1);
+
+            Assert.AreEqual("Test: 1.1", s);
+        }
+
+        #endregion
+    }
+}

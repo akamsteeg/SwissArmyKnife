6caeb93d8e97cdd9d5e9ee52acfd56e15493c923
Alex Kamsteeg <alex@atlex.nl>
2015-06-11 20:10:13 +0200

    Added tests for IsBetween()

--- src/SwissArmyKnife.Tests/GenericExtensionsTests.cs
+++ src/SwissArmyKnife.Tests/GenericExtensionsTests.cs
@@ -108,5 +108,50 @@ namespace SwissArmyKnife.Tests
         }
 
         #endregion
+
+        #region IsBetween()
+
+        [Test]
+        public void StringIsBetween_Successful()
+        {
+            var t1 = "b".IsBetween("a", "c");
+            var t2 = "z".IsBetween("a", "c");
+
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+        }
+
+        [Test]
+        public void IntIsBetween_Successful()
+        {
+            var t1 = 1.IsBetween(0, 2);
+            var t2 = 9.IsBetween(0, 2);
+
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+        }
+
+        [Test]
+        public void DoubleIsBetween_Successful()
+        {
+            var t1 = 1.1.IsBetween(1.0, 1.5);
+            var t2 = 9.0.IsBetween(1.0, 2.0);
+
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+        }
+
+        [Test]
+        public void IsBetweenNullValues_Successful()
+        {
+            var t1 = "b".IsBetween(null, "c");
+            var t2 = "b".IsBetween("a", null);
+            var t3 = "b".IsBetween(null, null);
+
+            Assert.IsTrue(t1);
+            Assert.IsFalse(t2);
+            Assert.IsFalse(t3);
+        }
+        #endregion
     }
 }
--- src/SwissArmyKnife/GenericExtensions.cs
+++ src/SwissArmyKnife/GenericExtensions.cs
@@ -71,7 +71,7 @@ namespace SwissArmyKnife
         /// <param name="upper">The upper bound</param>
         /// <returns>True if the value is between or equal to the lower or upper value, false otherwise</returns>
         public static bool IsBetween<T>(this T value, T lower, T upper) where T: IComparable<T> =>
-            (value.CompareTo(lower) >= 0 || value.CompareTo(upper) <= 0);
+            (value.CompareTo(lower) >= 0 && value.CompareTo(upper) <= 0);
 
         #endregion
     }

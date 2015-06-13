fcf39002bda7b3042af964b6e3156380cb54e778
Alex Kamsteeg <alex@atlex.nl>
2015-06-13 13:50:01 +0200

    Added tests for the stream.Reset() extension method

---
+++ src/SwissArmyKnife.Tests/StreamExtensionTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace SwissArmyKnife.Tests
+{
+    [TestFixture]
+    internal class StreamExtensionTests
+    {
+        [Test]
+        public void Reset_Successful()
+        {
+            var s = GetStream();
+
+            s.Reset();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void ResetDisposedStream()
+        {
+            var s = GetStream();
+            s.Dispose();
+
+            s.Reset();
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void ResetClosedStream()
+        {
+            var s = GetStream();
+            s.Close();
+
+            s.Reset();
+        }
+
+        private static Stream GetStream()
+        {
+            const int size = 65535;
+
+            var newStream = new MemoryStream();
+
+            var data = new byte[size];
+
+            Random r = new Random();
+            r.NextBytes(data);
+
+            newStream.Write(data, 0, size);
+
+            return newStream;
+        }
+    }
+}

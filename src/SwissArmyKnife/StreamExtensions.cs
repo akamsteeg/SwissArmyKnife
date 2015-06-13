2baf9c65a8c318180154680c5b20162c866a0c82
Alex Kamsteeg <alex@atlex.nl>
2015-06-13 13:15:14 +0200

    Added the stream.Reset() extension method

--- src/SwissArmyKnife/StreamExtensions.cs
+++ src/SwissArmyKnife/StreamExtensions.cs
@@ -65,5 +65,20 @@ namespace SwissArmyKnife
             }
         }
 #endif
+
+        /// <summary>
+        /// Reset the position within the stream 
+        /// back to the beginning
+        /// </summary>
+        /// <param name="source"></param>
+        public static void Reset(this Stream source)
+        {
+            if (!source.CanRead && !source.CanWrite)
+                throw new ObjectDisposedException(nameof(source));
+            if (!source.CanSeek)
+                throw new NotSupportedException("Cannot seek in the stream");
+
+            source.Seek(0, SeekOrigin.Begin);
+        }
     }
 }

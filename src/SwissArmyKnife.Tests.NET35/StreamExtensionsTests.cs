0ef1b9361d92b0974578f4eff55f949f78f75be9
Alex Kamsteeg <alex@atlex.nl>
2015-06-13 12:43:24 +0200

    Added basic unit tests for the stream.CopyTo() extension method

---
+++ src/SwissArmyKnife.Tests.NET35/StreamExtensionsTests.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwissArmyKnife;
+using System.IO;
+
+namespace SwissArmyKnife.Tests.NET35
+{
+    [TestFixture]
+    internal class StreamExtensionsTests
+    {
+        private const int SIZE = 1024;
+
+        private Stream SourceStream
+        {
+            get; set;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.SourceStream = new MemoryStream();
+
+            var data = new byte[SIZE];
+
+            Random r = new Random();
+            r.NextBytes(data);
+
+            this.SourceStream.Write(data, 0, SIZE);
+        }
+
+        [Test]
+        public void CopyToEmptyStream_Sucessful()
+        {
+            MemoryStream destination = new MemoryStream();
+
+            this.SourceStream.CopyTo(destination);
+
+            Assert.IsNotNull(destination);
+            Assert.AreEqual(SIZE, destination.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CopyToNullStream_Throws()
+        {
+            MemoryStream destination = null;
+
+            this.SourceStream.CopyTo(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void CopyToDisposedStream_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+            destination.Dispose();
+
+            this.SourceStream.CopyTo(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void CopyToClosedStream_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+            destination.Close();
+
+            this.SourceStream.CopyTo(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void CopyFromDisposedStream_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+
+            this.SourceStream.Dispose();
+
+            this.SourceStream.CopyTo(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void CopyFromClosedStream_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+            
+            this.SourceStream.Close();
+
+            this.SourceStream.CopyTo(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CopyToWithZeroBufferSize_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+
+            this.SourceStream.CopyTo(destination, 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CopyToWithNegativeBufferSize_Throws()
+        {
+            MemoryStream destination = new MemoryStream();
+
+            this.SourceStream.CopyTo(destination, -1);
+        }
+    }
+}

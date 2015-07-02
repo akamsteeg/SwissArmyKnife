25d97853d55e47444ffe17a3efe120ea2b9d15bd
Alex Kamsteeg <alex@atlex.nl>
2015-07-02 20:58:08 +0200

    Added Stopwatch.GetValueAndReset()

---
+++ src/SwissArmyKnife/StopwatchExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SwissArmyKnife
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StopwatchExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sw"></param>
+        /// <returns></returns>
+        public static TimeSpan GetValueAndReset(this Stopwatch sw)
+        {
+            sw.Stop();
+            var result = sw.Elapsed;
+
+            sw = Stopwatch.StartNew();
+
+            return result;
+        }
+
+    }
+}

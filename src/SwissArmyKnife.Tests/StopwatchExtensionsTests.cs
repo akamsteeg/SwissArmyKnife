cf2d360ab4ffb5890543af91fbb116204f16d987
Alex Kamsteeg <alex@atlex.nl>
2015-07-02 21:22:25 +0200

    Added tests for Stopwatch.GetValueAndReset()

---
+++ src/SwissArmyKnife.Tests/StopwatchExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests
+{
+    [TestFixture]
+    public class StopwatchExtensionsTests
+    {
+        [Test]
+        public void GetValueAndReset_Successful()
+        {
+            var sw = Stopwatch.StartNew();
+
+            Thread.Sleep(500);
+            var elapsed = sw.GetValueAndReset();
+
+            Assert.NotNull(elapsed);
+            Assert.Greater(elapsed.TotalMilliseconds, 250);
+
+            Assert.Less(500, sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
--- src/SwissArmyKnife/StopwatchExtensions.cs
+++ src/SwissArmyKnife/StopwatchExtensions.cs
@@ -26,6 +26,5 @@ namespace SwissArmyKnife
 
             return result;
         }
-
     }
 }

2f68314629bafcf2aa775a740fde0cf5a54608b7
Alex Kamsteeg <alex@atlex.nl>
2015-07-16 23:00:46 +0200

    Added unit tests for BloomFilter

---
+++ src/SwissArmyKnife.Tests/DataStructures/BloomFilterTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using SwissArmyKnife.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests.DataStructures
+{
+    [TestFixture]
+    internal class BloomFilterTests
+    {
+        [Test]
+        public void AddAndTest_Successful([Values("a", "b", "c", "d", "lorem ipsum dolor sit amet")]string input)
+        {
+            var bf = new BloomFilter(64);
+
+            var testData = Encoding.ASCII.GetBytes(input);
+
+            bf.Add(testData);
+
+            var result = bf.Test(testData);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestWithoutAdd_Successful([Values("a", "b", "c", "d", "lorem ipsum dolor sit amet")]string input)
+        {
+            var bf = new BloomFilter(64);
+
+            var testData = Encoding.ASCII.GetBytes(input);
+
+            var result = bf.Test(testData);
+
+            Assert.IsFalse(result);
+        }
+
+    }
+}

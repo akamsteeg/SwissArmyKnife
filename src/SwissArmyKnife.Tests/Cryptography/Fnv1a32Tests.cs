3dd723152b689b9cb2f224b376de8cf1326e37eb
Alex Kamsteeg <alex@atlex.nl>
2015-07-16 18:44:11 +0200

    Added unit tests for 32b FNV

---
+++ src/SwissArmyKnife.Tests/Cryptography/Fnv1a32Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using SwissArmyKnife.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests.Cryptography
+{
+    [TestFixture]
+    internal class Fnv1a32Tests : Fnv1aTestsBase
+    {
+        public Fnv1a32Tests()
+            : base(new Fnv1a32())
+        {
+
+        }
+
+        [Test, Sequential]
+        public void Fnv_Succesful(
+            [Values("fnv", "Fowler/Noll/Vo", "fowler", "noll", "vo")]string input,
+            [Values((uint)3002452889, (uint)2281434366, (uint)261441234, (uint)2293073354, (uint)1112130898)]uint expected
+            )
+        {
+            byte[] dataToHash = Encoding.UTF8.GetBytes(input);
+            byte[] hashedData = this.FnvHash.ComputeHash(dataToHash);
+
+            uint result = BitConverter.ToUInt32(hashedData, 0);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ src/SwissArmyKnife.Tests/Cryptography/Fnv1aTestsBase.cs
@@ -0,0 +1,24 @@
+﻿using SwissArmyKnife.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests.Cryptography
+{
+    internal abstract class Fnv1aTestsBase
+    {
+        protected Fnv1aBase FnvHash
+        {
+            get;
+            private set;
+        }
+
+        public Fnv1aTestsBase(Fnv1aBase fnvHash)
+        {
+            this.FnvHash = fnvHash;
+        }
+
+    }
+}

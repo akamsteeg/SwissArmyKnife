b68080e01e29b465eed4dc150505d1c6b896b640
Alex Kamsteeg <alex@atlex.nl>
2015-07-16 18:29:26 +0200

    Added documentation for the FNV 32b implementation

--- src/SwissArmyKnife/Cryptography/Fnv1a32.cs
+++ src/SwissArmyKnife/Cryptography/Fnv1a32.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace SwissArmyKnife.Cryptography
 {
-    class Fnv1a32
+    /// <summary>
+    /// An Fowler-Noll-Vo 1a implementation, for 32 bits
+    /// </summary>
+    /// <remarks>
+    /// FNV is not intended to be a cryptographically safe
+    /// hash function
+    /// </remarks>
+    public sealed class Fnv1a32 : Fnv1aBase
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Fnv1a32"/>
+        /// </summary>
+        public Fnv1a32()
+            : base(unchecked(2166136261), unchecked(16777619))
+        {
+            this.HashSizeValue = 32;
+        }
     }
 }
--- src/SwissArmyKnife/Cryptography/Fnv1aBase.cs
+++ src/SwissArmyKnife/Cryptography/Fnv1aBase.cs
@@ -7,8 +7,12 @@ using System.Text;
 namespace SwissArmyKnife.Cryptography
 {
     /// <summary>
-    /// 
+    /// An base class for Fowler-Noll-Vo 1a implementations
     /// </summary>
+    /// <remarks>
+    /// FNV is not intended to be a cryptographically safe
+    /// hash function
+    /// </remarks>
     public abstract class Fnv1aBase : HashAlgorithm
     {
         private ulong _offsetBasis;
@@ -16,10 +20,16 @@ namespace SwissArmyKnife.Cryptography
         private ulong _hashData;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of <see cref="Fnv1aBase"/>
         /// </summary>
-        /// <param name="offsetBasis"></param>
-        /// <param name="fnvPrime"></param>
+        /// <param name="offsetBasis">
+        /// The offset basis for the Fowler-Noll-Vo
+        /// algorithm
+        /// </param>
+        /// <param name="fnvPrime">
+        /// The prime to use in the Fowler-Noll-Vo
+        /// algorithm
+        /// </param>
         internal Fnv1aBase(ulong offsetBasis, ulong fnvPrime)
         {
             this._offsetBasis = offsetBasis;
@@ -27,11 +37,20 @@ namespace SwissArmyKnife.Cryptography
         }
 
         /// <summary>
-        /// 
+        /// Routes data written to the object into the hash 
+        /// algorithm for computing the hash
         /// </summary>
-        /// <param name="array"></param>
-        /// <param name="ibStart"></param>
-        /// <param name="cbSize"></param>
+        /// <param name="array">
+        /// The input to compute the hash code for
+        /// </param>
+        /// <param name="ibStart">
+        /// The offset into the byte array from which to begin 
+        /// using data
+        /// </param>
+        /// <param name="cbSize">
+        /// The number of bytes in the byte array to use as 
+        /// data
+        /// </param>
         protected sealed override void HashCore(byte[] array, int ibStart, int cbSize)
         {
             this._hashData = this._offsetBasis;
@@ -47,9 +66,12 @@ namespace SwissArmyKnife.Cryptography
         }
 
         /// <summary>
-        /// 
+        /// Finalizes the hash computation after the last data 
+        /// is processed by the cryptographic stream object
         /// </summary>
-        /// <returns></returns>
+        /// <returns>
+        /// The computed hash code
+        /// </returns>
         protected sealed override byte[] HashFinal()
         {
             var result = BitConverter.GetBytes(this._hashData);
@@ -58,11 +80,12 @@ namespace SwissArmyKnife.Cryptography
         }
 
         /// <summary>
-        /// 
+        /// Initializes this implementation
+        /// of the <see cref="HashAlgorithm"/>
+        /// class
         /// </summary>
         public sealed override void Initialize()
         {
         }
     }
-}
 }

e8f666295c54c3c839875517b7ed2ea79cbae470
Alex Kamsteeg <alex@atlex.nl>
2015-07-17 19:46:19 +0200

    Added another overload to string.FormatWith() so we only drop back to an overload with a params[] parameter by 5 or more parameters (#1)

--- src/SwissArmyKnife/Extensions/StringExtensions.cs
+++ src/SwissArmyKnife/Extensions/StringExtensions.cs
@@ -70,6 +70,32 @@ namespace SwissArmyKnife.Extensions
             string.Format(source, arg0, arg1, arg2);
 
         /// <summary>
+        /// Replaces the format items in a specified string with the string
+        /// representation of three specified objects
+        /// </summary>
+        /// <param name="source">
+        /// A composite format string
+        /// </param>
+        /// <param name="arg0">
+        /// The first object to format
+        /// </param>
+        /// <param name="arg1">
+        /// The second object to format
+        /// </param>
+        /// <param name="arg2">
+        /// The third object to format
+        /// </param>
+        /// <param name="arg3">
+        /// The fourth object to format
+        /// </param>
+        /// <returns>
+        /// A copy of the string in which any format items are replaced by the
+        /// string representation of arg0, arg1 and arg2
+        /// </returns>
+        public static string FormatWith(this string source, object arg0, object arg1, object arg2, object arg3) =>
+            string.Format(source, arg0, arg1, arg2, arg3);
+
+        /// <summary>
         /// Replaces the format item in a specified string with the string
         /// representation of a corresponding object in a specified array
         /// </summary>

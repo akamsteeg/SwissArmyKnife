cb03e6d3048093afea10c8c51685307f59d08f22
Alex Kamsteeg <alex@atlex.nl>
2015-07-25 12:22:36 +0200

    Added basic tests for SimpleObjectPool

---
+++ src/SwissArmyKnife.Tests/Mocks/IntPoolableObject.cs
@@ -0,0 +1,23 @@
+﻿using SwissArmyKnife.Pools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests.Mocks
+{
+    public class IntPoolableObject : PoolableObject
+    {
+        public int Data
+        {
+            get;
+            set;
+        }
+
+        protected override void Reset()
+        {
+            this.Data = 0;
+        }
+    }
+}
---
+++ src/SwissArmyKnife.Tests/Mocks/StringPoolableObject.cs
@@ -0,0 +1,23 @@
+﻿using SwissArmyKnife.Pools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissArmyKnife.Tests.Mocks
+{
+    public class StringPoolableObject : PoolableObject
+    {
+        public string Data
+        {
+            get;
+            set;
+        }
+
+        protected override void Reset()
+        {
+            Data = null;
+        }
+    }
+}

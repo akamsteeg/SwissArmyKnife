b6aca0e219ed79e043d9e7656b67283b6c0df161
Alex Kamsteeg <alex@atlex.nl>
2015-08-08 16:24:21 +0200

    Updated documentation for IComparableExtensions.IsBetween()
    IsBetween() is inclusive, it checks whether the value is equal to or between the supplied values. The documentation wasn't clear on that.

--- src/SwissArmyKnife/Extensions/IComparableExtensions.cs
+++ src/SwissArmyKnife/Extensions/IComparableExtensions.cs
@@ -13,12 +13,21 @@ namespace SwissArmyKnife.Extensions
         #region IsBetween()
 
         /// <summary>
-        /// Checks whether the object is between the supplied upper and lower values
+        /// Checks whether the object is equal to or between the supplied upper
+        /// and lower values
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="lower">The lower bound</param>
-        /// <param name="upper">The upper bound</param>
-        /// <returns>True if the value is between or equal to the lower or upper value, false otherwise</returns>
+        /// <param name="value">
+        /// </param>
+        /// <param name="lower">
+        /// The lower bound
+        /// </param>
+        /// <param name="upper">
+        /// The upper bound
+        /// </param>
+        /// <returns>
+        /// True if the value is between or equal to the lower or upper value,
+        /// false otherwise
+        /// </returns>
         public static bool IsBetween<T>(this T value, T lower, T upper) where T : IComparable<T> =>
             (value.CompareTo(lower) >= 0 && value.CompareTo(upper) <= 0);
 

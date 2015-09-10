2ce1cfc2c412349fcc0e622d19b7a5333b238536
Alex Kamsteeg <alex@atlex.nl>
2015-09-10 13:37:36 +0200

    Added a base class for Disposable implementations
    The Disposable class acts as a base class for classes that would normally implement IDisposable. It takes away all the tedious work normally associated with manually implementing the Disposable pattern and is thread-safe too

---
+++ src/SwissArmyKnife/Disposable.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Threading;
+
+namespace SwissArmyKnife
+{
+    /// <summary>
+    /// Implements the Disposable pattern and is used
+    /// as a base class for disposable resources
+    /// </summary>
+    /// <remarks>
+    /// This class is thread-safe
+    /// </remarks>
+    public abstract class Disposable : IDisposable
+    {
+        /// <summary>
+        /// Holds the value to track the disposed state. As
+        /// long as the value is 0 the object is not disposed
+        /// </summary>
+        private int _disposed;
+
+        /// <summary>
+        /// Gets the state of the disposable object
+        /// </summary>
+        protected bool IsDisposed
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Disposable"/>
+        /// </summary>
+        protected Disposable()
+        {
+            this._disposed = 0;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with 
+        /// freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (Interlocked.CompareExchange(ref this._disposed, 1, 0) == 0)
+            {
+                this.Dispose(true);
+                /*
+                 * We cannot simply let the getter of IsDisposed check for the value of
+                 * this._disposed, because it would be true after the 
+                 * Interlocked.CompareExchange(). Any Dispose(bool) implementations 
+                 * checking for the value of  IsDisposed to avoid double disposing a 
+                 * managed resource would fail then.
+                 */
+                this.IsDisposed = true;
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with 
+        /// freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// True when disposing, false when finalizing
+        /// </param>
+        protected abstract void Dispose(bool disposing);
+
+        /// <summary>
+        /// Utility method for throwing a <see cref="ObjectDisposedException"/>
+        /// when this <see cref="Disposable"/> object is disposed
+        /// </summary>
+        protected void ThrowIfDisposed()
+        {
+            if (this.IsDisposed)
+                throw new ObjectDisposedException(this.GetType().FullName);
+        }
+    }
+}

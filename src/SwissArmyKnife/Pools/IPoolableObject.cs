2a9a75e64d3946d83b5445afc3e22e2f6f29f189
Alex Kamsteeg <akamsteeg@cyclomedia.com>
2016-04-21 14:16:01 +0200

    Updated documentation

--- src/SwissArmyKnife/Pools/IPoolableObject.cs
+++ src/SwissArmyKnife/Pools/IPoolableObject.cs
@@ -17,6 +17,16 @@ namespace SwissArmyKnife.Pools
         /// Performs application-defined tasks associated with freeing, releasing, or resetting
         /// unmanaged resources and return it to the <see cref="IObjectPool"/>
         /// </summary>
+        /// <example>
+        /// public void Dispose()
+        /// {
+        ///     // Reset the object for reuse
+        ///     
+        ///     // Return it to the pool
+        ///     GC.SuppressFinalize(this);
+        ///     this.Pool.Return(this);
+        /// }
+        /// </example>
         new void Dispose();
     }
 }

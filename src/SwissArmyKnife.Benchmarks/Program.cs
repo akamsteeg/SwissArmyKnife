394fc8aa109e723cd3bcc7504f45982108f4a0c4
Alex Kamsteeg <alex@atlex.nl>
2016-07-20 12:11:04 +0200

    Removed MiniBench and replaced it with BenchmarkDotNet

--- src/SwissArmyKnife.Benchmarks/Program.cs
+++ src/SwissArmyKnife.Benchmarks/Program.cs
@@ -1,27 +1,52 @@
-﻿using Minibench.Framework;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using SwissArmyKnife.Benchmarks.Benches.DataStructures;
+using SwissArmyKnife.Benchmarks.Benches.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SwissArmyKnife.Benchmarks
 {
     class Program
     {
-        private static int Main(string[] args)
+        private static void Main(string[] args)
         {
-            Stopwatch sw = Stopwatch.StartNew();
-            var run = BenchmarkRunner.RunFromCommandLine(typeof(Program).Assembly);
-            sw.Stop();
+            var config = GetConfig();
+            var benchmarks = GetBenchmarks();
 
-            Console.WriteLine();
-            Console.WriteLine("Running all tests took {0} seconds".FormatWith(sw.Elapsed.TotalSeconds));
-            Console.WriteLine("Press a key to exit");
-            Console.ReadKey();
+            for (var i = 0; i < benchmarks.Length; i++)
+            {
+                var typeToRun = benchmarks[i];
+                BenchmarkRunner.Run(typeToRun, config);
+            }
 
-            return run == null ? 1 : 0;
+            //BenchmarkRunner.Run<StringExtensionsBenchmarks>(config);
+        }
+
+        private static Type[] GetBenchmarks()
+        {
+            var result = new Type[]
+            {
+                // Extensions
+                typeof(GenericExtensionsBenchmarks),
+                typeof(StringBuilderExtensionsBenchmarks),
+                typeof(StringExtensionsBenchmarks),
+                //Data structures
+                typeof(BloomFilterBenchmarks),
+            };
+
+            return result;
+        }
+
+        private static IConfig GetConfig()
+        {
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            var gcDiagnoser = new MemoryDiagnoser();
+            config.Add(new Job { Mode = Mode.Throughput, LaunchCount = 2, WarmupCount = 2, TargetCount = 10 });
+            config.Add(gcDiagnoser);
+
+            return config;
         }
     }
 }

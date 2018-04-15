e957e5c845718a4271719613914033a5d7693fcd
Alex Kamsteeg <alex@atlex.nl>
2018-04-15 15:07:38 +0200

    Introduced an IBloomFilter interface to make overriding the BloomFilter implementation easier

---
+++ src/SwissArmyKnife/DataStructures/IBloomFilter.cs
@@ -0,0 +1,26 @@
+﻿namespace SwissArmyKnife.DataStructures
+{
+    /// <summary>
+    /// Represents a Bloom filter data structure
+    /// </summary>
+    public interface IBloomFilter
+    {
+        /// <summary>
+        /// Add data to the filter
+        /// </summary>
+        /// <param name="input">
+        /// The data to add
+        /// </param>
+        void Add(byte[] input);
+
+        /// <summary>
+        /// Test whether the input is probably
+        /// in the filter, or absolutely not
+        /// </summary>
+        /// <returns>
+        /// True if the data is probably in the
+        /// filter, false otherwise
+        /// </returns>
+        bool Test(byte[] input);
+    }
+}

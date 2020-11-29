7b43c8f6a0b3bcc69d61ae303f3551ed0be59870
Alex Kamsteeg <alex@atlex.nl>
2020-11-29 14:57:37 +0100

    Add equalityhelper to simplify implementations of IEquetable<T> (#33)

---
+++ src/SwissArmyKnife.Tests/Helpers/EqualityHelperTests.cs
@@ -0,0 +1,64 @@
+﻿using SwissArmyKnife.Helpers;
+using Xunit;
+
+namespace SwissArmyKnife.Tests.Helpers
+{
+    public class EqualityHelperTests
+    {
+        [Fact]
+        public void EqualsT_WithOtherNull_ReturnsFalse()
+        {
+            var result = EqualityHelper.Equals(this, (object)null);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualsT_WithOtherDifferentObject_ReturnsFalse()
+        {
+            var result = EqualityHelper.Equals(this, new object());
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualsT_WithSameObject_ReturnsTrue()
+        {
+            var o = "Dummy";
+
+            var result = EqualityHelper.Equals(o, (object)o);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void EqualsTT_WithOtherNull_ReturnsFalse()
+        {
+            var result = EqualityHelper.Equals("Dummy", (string)null);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualsTT_WithOtherDifferentObject_ReturnsFalse()
+        {
+            var a = "A";
+
+            var b = "B";
+
+            var result = EqualityHelper.Equals(a, b);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void EqualsTT_WithOtherSameObject_ReturnsTrue()
+        {
+            var o = "Dummy";
+
+            var result = EqualityHelper.Equals(o, o);
+
+            Assert.True(result);
+        }
+    }
+}
